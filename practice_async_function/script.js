771f437fce923b24013b9d09b33349921df5d137
maneesh84 <maneeshkumar849896@gmail.com>
2025-07-26 15:52:44 +0530

    two async practice_async_function

--- practice_async_function/script.js
+++ practice_async_function/script.js
@@ -6,6 +6,18 @@ function asyncfunc1(){
     }, 1000)
 })
 }
+function async2(){
+  return new Promise(
+    (resolved,reject)=>{
+    setTimeout(()=>{
+      console.log("Async function 2 executed");
+      resolved("Result from async function 2");
+    },1000)
+    }
+  )
+}
 
 let promise1=asyncfunc1();
 console.log(promise1);
+let promise2=async2();
+console.log(promise2);
